cd02975068fdcce025a54a2ff7408d30fd7dd511
masonentrican <mason@entrican.com>
2018-12-05 12:57:46 -0800

    Tutorial Finished
    Bolt client/server with robot and mecanim working.

---
+++ Assets/Resources/Scripts/CubeBehaviour.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : Bolt.EntityEventListener<ICubeState>
+{
+    public GameObject[] WeaponObjects;
+    float resetColorTime;
+    Renderer renderer;
+
+    public override void Attached()
+    {
+        renderer = GetComponent<Renderer>();
+
+        state.SetTransforms(state.CubeTransform, transform);
+
+        if (entity.isOwner)
+        {
+            // setup cube color for our cube
+            state.CubeColor = new Color(Random.value, Random.value, Random.value);
+
+            // setup weapons
+            for (int i = 0; i < state.WeaponArray.Length; ++i)
+            {
+                state.WeaponArray[i].WeaponId = i;
+                state.WeaponArray[i].WeaponAmmo = Random.Range(50, 100);
+            }
+
+            // by default we dont have a weapon active, index -1 defines that.
+            state.WeaponActiveIndex = -1;
+        }
+
+        // Setup cube colorchange callback
+        state.AddCallback("CubeColor", ColorChanged);
+
+        // Setup active weapon callback
+        state.AddCallback("WeaponActiveIndex", WeaponActiveIndexChanged);
+    }
+
+    public override void SimulateOwner()
+    {
+        var speed = 4f;
+        var movement = Vector3.zero;
+
+        // movement
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        // input for weapon selection
+        if (Input.GetKeyDown(KeyCode.Alpha0)) { state.WeaponActiveIndex = -1; }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { state.WeaponActiveIndex = 0;  }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { state.WeaponActiveIndex = 1;  }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { state.WeaponActiveIndex = 2;  }
+
+        
+        // cube flashing input
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            var flash = FlashColorEvent.Create(entity);
+            flash.FlashColor = Color.red;
+            flash.Send();
+        }
+
+        // actual transform movement
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.frameDeltaTime);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (entity.isOwner)
+        {
+            GUI.color = state.CubeColor;
+            GUILayout.Label("@@@");
+            GUI.color = Color.white;
+        }
+    }
+
+    public override void OnEvent(FlashColorEvent evnt)
+    {
+        resetColorTime = Time.time + 0.2f;
+        renderer.material.color = evnt.FlashColor;
+    }
+
+    void ColorChanged()
+    {
+        GetComponent<Renderer>().material.color = state.CubeColor;
+    }
+
+    void WeaponActiveIndexChanged()
+    {
+        // disable active for all weapons
+        for (int i = 0; i < WeaponObjects.Length; ++i)
+        {
+            WeaponObjects[i].SetActive(false);
+        }        
+
+        // if a weapon has an active index that is >= 0 set the weapon object to active
+        if (state.WeaponActiveIndex >= 0)
+        {
+            int objectId = state.WeaponArray[state.WeaponActiveIndex].WeaponId;
+            WeaponObjects[objectId].SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (resetColorTime < Time.time)
+        {
+            renderer.material.color = state.CubeColor;
+        }
+    }
+}
+
+
--- Assets/Resources/Scripts/NetworkCallbacks.cs
+++ Assets/Resources/Scripts/NetworkCallbacks.cs
@@ -5,12 +5,46 @@ using UnityEngine;
 [BoltGlobalBehaviour]
 public class NetworkCallbacks : Bolt.GlobalEventListener
 {
+    List<string> logMessages = new List<string>();
+
+
+    /// <summary>
+    /// Function runs on scene loading. Instantiates a cube prefab that has our control logic attached
+    /// </summary>
     public override void SceneLoadLocalDone(string map)
     {
         // Random spawn position
         var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
 
         // instantiate cube
-        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+        BoltNetwork.Instantiate(BoltPrefabs.Robot, spawnPosition, Quaternion.identity);
+    }
+
+
+    /// <summary>
+    /// Handles receiving events
+    /// </summary>    
+    public override void OnEvent(LogEvent evnt)
+    {
+        logMessages.Insert(0, evnt.Message);
+    }
+
+
+    /// <summary>
+    /// Create a small box and display 5 newest log messages within.
+    /// </summary>
+    void OnGUI()
+    {
+        // only display max the 5 latest log messages
+        int maxMessages = Mathf.Min(5, logMessages.Count);
+
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+
+        for (int i = 0; i < maxMessages; ++i)
+        {
+            GUILayout.Label(logMessages[i]);
+        }
+
+        GUILayout.EndArea();
     }
 }
