c709de6a758b08625c5a437907463404450124de
masonentrican <mason@entrican.com>
2018-12-05 10:24:12 -0800

    Initial Bolt Setup
    Basic Bolt integeration with Client/Server working with my AppId on PUN cloud.

---
+++ Assets/Resources/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // Random spawn position
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
